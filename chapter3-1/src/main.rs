99986e23b6ed8b6cc7f8ae0ead19476fe9b7c915
lijie <lijie@c5game.com>
2021-03-05 11:56:36 +0800

    chapter3.1

---
+++ chapter3-1/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let x=5;
+    println!("The value of x is {}",x);
+    // x=6;error
+    println!("The value of x is {}",x);
+    let y=5;
+    let y=y+1;
+    let y=y*2;
+
+    let mut spaces="     ";
+    // spaces=spaces.len(); //error
+}
